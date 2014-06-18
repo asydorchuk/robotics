d18ba3745efa62fc92a3b9e7c1765f5185433289
Andrii Sydorchuk <sydorchuk.andriy@gmail.com>
2014-06-18 23:55:04 +0200

    Add wheel encoder interaction to the prototype

--- python/robotics/prototype.py
+++ python/robotics/prototype.py
@@ -9,10 +9,17 @@ from sensors.redbot_wheel_encoder_sensor import RedbotWheelEncoderSensor
 def main():
   gpio.setmode(gpio.BOARD)
   lmotor = RedbotMotorActor(gpio, 8, 10, 12)
+  wencoder = RedbotWheelEncoderSensor()
+
+  print 'Number of left wheel ticks: %s' % wencoder.getLeftWheelTicks()
+  print 'Let\'s workout a bit'
+
   lmotor.start()
   lmotor.setPower(0.2)
   time.sleep(1)
   lmotor.stop()
+
+  print 'Number of left wheel ticks: %s' % wencoder.getLeftWheelTicks()
 
 if __name__ == '__main__':
   main()
