1e2f6629178840b9295bc22b23c2242a3aba4ceb
Andrii Sydorchuk <sydorchuk.andriy@gmail.com>
2014-06-19 22:48:11 +0200

    Update prototype to do operations with both wheels

--- python/robotics/prototype.py
+++ python/robotics/prototype.py
@@ -9,17 +9,28 @@ from sensors.redbot_wheel_encoder_sensor import RedbotWheelEncoderSensor
 def main():
   gpio.setmode(gpio.BOARD)
   lmotor = RedbotMotorActor(gpio, 8, 10, 12)
+  rmotor = RedbotMotorActor(gpio, 11, 13, 15)
   wencoder = RedbotWheelEncoderSensor()
 
   print 'Number of left wheel ticks: %s' % wencoder.getLeftWheelTicks()
+  print 'NUmber of right wheel ticks: %s' % wencoder.getRightWheelTicks()
   print 'Let\'s workout a bit'
 
   lmotor.start()
   lmotor.setPower(0.2)
+
+  rmotor.start()
+  rmotor.setPower(0.2)
+
   time.sleep(1)
+
   lmotor.stop()
+  rmotor.stop()
 
   print 'Number of left wheel ticks: %s' % wencoder.getLeftWheelTicks()
+  print 'Number of right wheel ticks: %s' % wencoder.getRightWheelTicks()
+
+  gpio.cleanup()
 
 if __name__ == '__main__':
   main()
