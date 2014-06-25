99c793a6f464ea028b45cb4a3bc88aec5269fc21
Andrii Sydorchuk <sydorchuk.andriy@gmail.com>
2014-06-25 21:32:38 +0200

    Making spi interface impl. PEP8 compliant

--- python/robotics/interfaces/spi/mcp3008_spi_interface.py
+++ python/robotics/interfaces/spi/mcp3008_spi_interface.py
@@ -3,12 +3,12 @@ import spidev
 
 class MCP3008SpiInterface(object):
 
-  def __init__(self, device_id):
-    self.device = spidev.SpiDev()
-    self.device.open(0, device_id)
-    self.device.max_speed_hz = 1000000
+    def __init__(self, device_id):
+        self.device = spidev.SpiDev()
+        self.device.open(0, device_id)
+        self.device.max_speed_hz = 1000000
 
-  def read(self, adc_id):
-    raw_data = self.device.xfer2([1, 8 + adc_id << 4, 0])
-    adc_out = ((raw_data[1] & 3) << 8) + raw_data[2] 
-    return adc_out
+    def read(self, adc_id):
+        raw_data = self.device.xfer2([1, 8 + adc_id << 4, 0])
+        adc_out = ((raw_data[1] & 3) << 8) + raw_data[2]
+        return adc_out
