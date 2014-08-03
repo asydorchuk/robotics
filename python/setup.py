b439694228f192d6ea7c20e419911b07f84690a2
Andrii Sydorchuk <sydorchuk.andriy@gmail.com>
2014-08-03 14:20:36 +0200

    Add setup script to the project

---
+++ python/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='slevin-robotics',
+      version='1',
+      author='Andrii Sydorchuk',
+      author_email='sydorchuk.andriy@gmail.com',
+      description='Robotics package',
+)
